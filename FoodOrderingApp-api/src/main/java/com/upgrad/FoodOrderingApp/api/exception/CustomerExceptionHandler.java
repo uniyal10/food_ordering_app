c34d67f1b0df90ece474b972b6f2c688ba2c744a
sudhanshu uniyal <uniyal1001@gmail.com>
2020-05-02 02:27:20 +0530

    handles user exception during signup and login

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/CustomerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.upgrad.FoodOrderingApp.api.exception;
+
+
+import com.upgrad.FoodOrderingApp.api.model.ErrorResponse;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomerExceptionHandler {
+
+
+    @ExceptionHandler(SignUpRestrictedException.class)
+    public ResponseEntity<ErrorResponse> signUpRestrictions(SignUpRestrictedException s, WebRequest webRequest) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(s.getCode()), HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> authFailedException(AuthenticationFailedException s, WebRequest webRequest) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(s.getCode()).message(s.getErrorMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+
+}
