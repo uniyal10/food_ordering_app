985061945ddecd8bfb00f783f9f7f6c234b4cfd5
sudhanshu uniyal <uniyal1001@gmail.com>
2020-05-02 20:48:08 +0530

    service business logic of address immplemented sucessfully

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/AddressService.java
@@ -0,0 +1,26 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AddressNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.SaveAddressException;
+
+import java.util.List;
+
+/*
+ * This AddressService interface gives the list of all the service that exist in the address service implementation class.
+ * Controller class will be calling the service methods by this interface.
+ */
+public interface AddressService {
+
+    AddressEntity saveAddress(AddressEntity addressEntity, CustomerAddressEntity customerAddressEntity) throws
+            SaveAddressException;
+    AddressEntity getAddressByUUID(String addressId, CustomerEntity customerEntity) throws AuthorizationFailedException,
+            AddressNotFoundException;
+    List<AddressEntity> getAllAddress(CustomerEntity customer);
+    StateEntity getState(String stateUUID);
+
+}
