96c91f091bf488dd989e072daa990fff19c00c59
sudhanshu uniyal <uniyal1001@gmail.com>
2020-05-03 06:39:44 +0530

    categoty Entity added sucessfully

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryEntity.java
@@ -0,0 +1,88 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CategoryEntity class contains all the attributes to be mapped to all the fields in category table in the database.
+ * All the annotations which are used to specify all the constraints to the columns in the database must be correctly implemented.
+ */
+@Entity
+@Table(name = "category")
+@NamedQueries({
+        @NamedQuery(name = "getAllCategoriesOrderedByName", query = "select c from CategoryEntity c order by c.categoryName ASC"),
+        @NamedQuery(name = "getCategoryById", query = "select c from CategoryEntity c where " +
+                "c.uuid = :uuid"),
+        @NamedQuery(name = "getCategoriesByRestaurant", query = "select c from CategoryEntity c inner join c.restaurants r where"
+                + " r.uuid = :uuid order by c.categoryName")
+})
+public class CategoryEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID", length = 64, nullable = false)
+    private String uuid;
+
+    @Column(name = "category_name", nullable = false)
+    private String categoryName;
+
+    @ManyToMany
+    @JoinTable(name = "category_item", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name =
+            "item_id"))
+    private List<ItemEntity> items = new ArrayList<>();
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "restaurant_category", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name =
+            "restaurant_id"))
+    private List<RestaurantEntity> restaurants = new ArrayList<>();
+
+    public CategoryEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<ItemEntity> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemEntity> items) {
+        this.items = items;
+    }
+
+    public List<RestaurantEntity> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<RestaurantEntity> restaurants) {
+        this.restaurants = restaurants;
+    }
+}
